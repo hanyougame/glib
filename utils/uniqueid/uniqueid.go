2b0c24ffe88302472a9e103f8670564d490f3bee
peizhengliang <peizhengliang@nggame888.com>
2025-01-08 14:34:20 +0800

    修改雪花id功能

--- utils/uniqueid/uniqueid.go
+++ utils/uniqueid/uniqueid.go
@@ -1,23 +1,44 @@
 package uniqueid
 
 import (
+	"fmt"
 	"github.com/sony/sonyflake"
-	"github.com/zeromicro/go-zero/core/logx"
+	"net"
 )
 
 var flake *sonyflake.Sonyflake
 
 func init() {
-	flake = sonyflake.NewSonyflake(sonyflake.Settings{})
+	flake = sonyflake.NewSonyflake(sonyflake.Settings{
+		MachineID: getMachineID,
+	})
 }
 
-func GenId() int64 {
+func GenId() (id uint64, err error) {
+	id, err = flake.NextID()
+	return
+}
 
-	id, err := flake.NextID()
+// 获取机器id
+func getMachineID() (uint16, error) {
+	addrList, err := net.InterfaceAddrs()
 	if err != nil {
-		logx.Severef("flake NextID failed with %s \n", err)
-		panic(err)
+		return 0, err
 	}
+	// 取第一个有效的非环回 IP 地址
+	for _, addr := range addrList {
+		if ip, ok := addr.(*net.IPNet); ok && !ip.IP.IsLoopback() {
+			return uint16(sum(ip.IP)), nil
+		}
+	}
+	return 0, fmt.Errorf("no valid IP address found")
+}
 
-	return int64(id)
+// 计算 IP 地址的和作为机器 ID
+func sum(ip net.IP) int {
+	total := 0
+	for _, b := range ip {
+		total += int(b)
+	}
+	return total % 1024 // 限制在最大机器 ID 范围内
 }
