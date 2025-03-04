a9ba05166553a3789b9f7f361287db0112eecce8
leo <leo@gmail.com>
2025-03-04 09:31:29 +0800

    简化代码

--- stores/userx/userx.go
+++ stores/userx/userx.go
@@ -18,13 +18,8 @@ type UserX struct {
 	rdb redis.UniversalClient //redis
 }
 
-func NewUserX() *UserX {
-	return &UserX{}
-}
-
-func (u *UserX) WithRdb(rdb redis.UniversalClient) *UserX {
-	u.rdb = rdb
-	return u
+func NewUserX(rdb redis.UniversalClient) *UserX {
+	return &UserX{rdb: rdb}
 }
 
 // StoreUserExceptionStatus 存储用户异常状态到Redis
@@ -32,49 +27,39 @@ func (u *UserX) WithRdb(rdb redis.UniversalClient) *UserX {
 // exceptionValue: 异常状态值
 // expireSeconds: 过期时间（秒），如果为0则使用默认过期时间
 // 返回: 存储是否成功，错误信息
-func (u *UserX) StoreUserExceptionStatus(ctx context.Context, userId, exceptionValue int64) (bool, error) {
+func (u *UserX) StoreUserExceptionStatus(ctx context.Context, userId, exceptionValue int64) error {
 	if userId == 0 || exceptionValue == 0 {
-		return false, fmt.Errorf("用户ID和异常状态值不能为空")
+		return fmt.Errorf("用户ID和异常状态值不能为空")
 	}
-
-	// 设置键名
-	key := fmt.Sprintf(KeyPrefix, userId)
-
-	// 设置过期时间(永久有效)
-
-	// 存储数据并设置过期时间
-	err := u.rdb.Set(ctx, key, exceptionValue, 0).Err()
-	if err != nil {
-		return false, fmt.Errorf("存储用户异常状态失败: %w", err)
-	}
-
-	return true, nil
+	return u.rdb.Set(ctx, fmt.Sprintf(KeyPrefix, userId), exceptionValue, 0).Err()
 }
 
 // GetUserExceptionStatus 从Redis获取用户异常状态
 // userId: 用户ID
 // 返回: 异常状态值，错误信息
-func (u *UserX) GetUserExceptionStatus(ctx context.Context, userId int64) (int64, error) {
+func (u *UserX) GetUserExceptionStatus(ctx context.Context, userId int64) (val int64, err error) {
 	if userId == 0 {
 		return 0, fmt.Errorf("用户ID不能为空")
 	}
-	// 设置键名
-	key := fmt.Sprintf(KeyPrefix, userId)
-
 	// 获取数据
-	value, err := u.rdb.Get(ctx, key).Result()
+	value, err := u.rdb.Get(ctx, fmt.Sprintf(KeyPrefix, userId)).Result()
 	if err != nil {
 		if errors.Is(err, redis.Nil) {
 			// 键不存在的情况
 			return 1, nil
 		}
 		// 其他错误
-		return 0, fmt.Errorf("获取用户异常状态失败: %w", err)
+		return
 	}
-	val, err := strconv.ParseInt(value, 10, 64)
-	if err != nil {
-		return 0, fmt.Errorf("获取用户异常状态失败: %w", err)
-	}
+	return strconv.ParseInt(value, 10, 64)
+}
 
-	return val, nil
+// DelUserExceptionStatus 从Redis删除用户异常状态
+// userId: 用户ID
+// 返回: 异常状态值，错误信息
+func (u *UserX) DelUserExceptionStatus(ctx context.Context, userId int64) error {
+	if userId == 0 {
+		return fmt.Errorf("用户ID不能为空")
+	}
+	return u.rdb.Del(ctx, fmt.Sprintf(KeyPrefix, userId)).Err()
 }
