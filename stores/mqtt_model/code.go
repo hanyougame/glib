3b4b5dff0fcf8d6717dbdfda405a4137278b026e
leo <leo@gmail.com>
2025-03-04 16:01:22 +0800

    增加mqtt code

--- stores/mqtt_model/code.go
+++ stores/mqtt_model/code.go
@@ -1,7 +1,9 @@
 package mqtt_model
 
 const (
-	UserPushOffCode = 1001 + iota // 用户被挤下线通知
+	UserRemoteLoginCode  = 1001 + iota // 用户异地登录通知
+	UserPushOffCode                    // 用户被挤下线通知
+	UserStatusChangeCode               // 用户状态变更
 )
 
 const (
