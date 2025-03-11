17ce205ced272c8670b12b6cb1b462b7512dcbb4
leo <leo@gmail.com>
2025-03-11 14:59:11 +0800

    修改sql hook

--- stores/gormx/database/trace.go
+++ stores/gormx/database/trace.go
@@ -34,8 +34,8 @@ func registerTraceHook(tx *gorm.DB) {
 
 func traceSql(spanName string, db *gorm.DB) {
 	sql := db.Dialector.Explain(db.Statement.SQL.String(), db.Statement.Vars...)
+	logx.WithContext(db.Statement.Context).Infof("spanName : %s, sql:%s", spanName, sql)
 	tracing.Inject(db.Statement.Context, spanName, func(span oteltrace.Span) oteltrace.Span {
-		logx.WithContext(db.Statement.Context).Infof("spanName : %s, sql:%s", spanName, sql)
 		span.SetAttributes(attribute.KeyValue{
 			Key:   "gorm.sql",
 			Value: attribute.StringValue(sql),
