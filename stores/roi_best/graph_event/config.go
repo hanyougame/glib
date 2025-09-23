a9972aed179631a2034759d1e4e5454af2072b3d
cc <carl@ogmail.org>
2025-09-23 16:23:30 +0800

    roibest埋点

--- stores/roi_best/graph_event/config.go
+++ stores/roi_best/graph_event/config.go
@@ -8,5 +8,5 @@ type RBConfig struct {
 	RBLinkID     string        `json:"link_id"`     //由系统生成的链接 ID
 	RBEventsName []RBEventName `json:"events_name"` // 事件名称
 	RBChannelID  string        `json:"channel_id"`  // 埋点平台渠道
-	RBRbPixelID  string        `json:"rb_pixel_id"` // 像素ID
+	RBRbPixelID  string        `json:"pixel_id"`    // 像素ID
 }
