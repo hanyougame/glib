6eb94b0fdf8a4605793ca078522f6a20a121e8aa
cc <carl@ogmail.org>
2025-09-23 16:49:09 +0800

    roibest埋点

--- stores/roi_best/graph_event/types.go
+++ stores/roi_best/graph_event/types.go
@@ -7,3 +7,18 @@ const (
 	RBEventNameRecharge      RBEventName = "Purchase"             // 充值（标准事件）
 	RBEventNameRechargeFirst RBEventName = "AddToCart"            // 首充（标准事件）
 )
+
+type ChannelID string
+
+const (
+	ChannelIDFacebook      ChannelID = "4"
+	ChannelIDTiktok        ChannelID = "5"
+	ChannelIDKwai          ChannelID = "9"
+	ChannelIDGoogle        ChannelID = "10"
+	ChannelIDXiaoBuWangLuo ChannelID = "20"
+	ChannelIDOKSPIN        ChannelID = "21"
+	ChannelIDSNAPTUBE      ChannelID = "22"
+	ChannelIDBIGO          ChannelID = "23"
+	ChannelIDAPPLUCK       ChannelID = "24"
+	ChannelIDTrafficstars  ChannelID = "34"
+)
