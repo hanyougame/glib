9ba19cf9d177112964fccb0a630aec4f5e7fddb0
cc <carl@ogmail.org>
2025-09-24 10:07:49 +0800

    roibest埋点

--- stores/roi_best/graph_event/config.go
+++ stores/roi_best/graph_event/config.go
@@ -1,5 +1,11 @@
 package graph_event
 
+import (
+	"database/sql/driver"
+	"fmt"
+	"github.com/zeromicro/go-zero/core/jsonx"
+)
+
 const (
 	FBEventApiUrl = "https://sdk-report.roibestopenapi.com/report/fb/event" // api url
 )
@@ -9,4 +15,30 @@ type RBConfig struct {
 	RBEventsName []RBEventName `json:"rb_events_name"` // 事件名称
 	RBChannelID  string        `json:"rb_channel_id"`  // 埋点平台渠道
 	RBRbPixelID  string        `json:"rb_pixel_id"`    // 像素ID
+}
+
+func (s *RBConfig) Scan(value interface{}) error {
+	var (
+		err    error
+		result RBConfig
+	)
+
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("invalid config val: %+v", value)
+	}
+	if err = jsonx.Unmarshal(bytes, &result); err != nil {
+		return fmt.Errorf("decode config err: %+v", err)
+	}
+	*s = result
+
+	return nil
+}
+
+func (s *RBConfig) Value() (driver.Value, error) {
+	if s == nil {
+		return nil, nil
+	}
+
+	return jsonx.Marshal(s)
 }
